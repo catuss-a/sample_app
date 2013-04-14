2b8c7318a2f3e5c13eef662b4795f2b9664b5c1a
catuss-a <axel.catusse@gmail.com>
2013-04-15 00:29:56 +0200

    :D

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Sample_App" }
+
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
@@ -11,7 +14,7 @@ describe "StaticPages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Sample_App | Home")
+                        :text => "#{base_title} | Home")
     end
   end
 
@@ -25,7 +28,7 @@ describe "StaticPages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                :text => "Sample_App | Help")
+                                :text => "#{base_title} | Help")
     end
   end
 
@@ -39,7 +42,7 @@ describe "StaticPages" do
     it "should have the title 'About'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                                :text => "Sample_App | About")
+                                :text => "#{base_title} | About")
     end
   end
 
@@ -53,7 +56,7 @@ describe "StaticPages" do
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-                                :text => "Sample_App | Contact")
+                                :text => "#{base_title} | Contact")
     end
   end
 end
